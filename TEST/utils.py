91a2f365d61813136c983fa16c66ec230b6ac1a1
24GUNV <38719890+24GUNV@users.noreply.github.com>
2022-09-19 21:28:47 +0700

    Add function to convert RGB to HSV
    Only works from far distance currently

--- TEST/utils.py
+++ TEST/utils.py
@@ -3,21 +3,49 @@
 
 # Function to sense far colors
 def sense_color_far(color_sensor):
-    hsv = color_sensor.hsv(surface =True)
-    if hsv.s < 0.22:
-        if hsv.v >= 25:
+    colors = color_sensor.rgb()
+    hsv = RGB_to_HSV(colors[0], colors[1], colors[2])
+    if hsv[1] < 0.22:
+        if hsv[2] >= 25:
             color = 6
         else:
             color = 5
     
     else:
-        if (hsv.h >= 300 and hsv.h <= 360) or hsv < 20:
+        if (hsv[0] >= 300 and hsv[0] <= 360) or hsv < 20:
             color = 1
-        elif hsv.h >= 20 and hsv.h < 90:
+        elif hsv[0] >= 20 and hsv[0] < 90:
             color = 2
-        elif hsv.h >= 90 and hsv.h < 210:
+        elif hsv[0] >= 90 and hsv[0] < 210:
             color = 3
         else:
             color = 4
     
     return color
+
+# Given R G and B colors, returns the hue saturation and value (hsv)
+def RGB_to_HSV(r, g, b):
+    min_num = min(min(r, g), b)
+    max_num = max(max(r, g), b)
+
+    if max_num == min_num:
+        h = 0
+    elif max == r and g == b:
+        h = 60 * (g - b) / (max_num - min_num)
+    elif max_num == r and g < b:
+        h = 60 * (g - b) / (max_num - min_num) + 360
+    elif max_num == g:
+        h = 60 * (b - r) / (max_num - min_num) + 120
+    elif max_num == b:
+        h = 60 * (r - g) / (max_num - min_num) + 240
+    
+    l = (max_num + min_num) / 2
+
+    if max_num == 0:
+        s = 0
+    else:
+        s = 1 - min_num / max_num
+
+    v = max_num
+
+    return (h, s, v)
